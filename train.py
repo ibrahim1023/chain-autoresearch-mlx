e8ff757dd0ad08f2d7bc5dfb99fbaa0e0ccaf81f
Ibrahim Arshad <38925737+ibrahim1023@users.noreply.github.com>
2026-03-31 16:57:51 +0400

    chore: discard longer upper-layer attention run

--- train.py
+++ train.py
@@ -358,7 +358,7 @@ class AdamW:
 # Model architecture
 ASPECT_RATIO = 64
 HEAD_DIM = 128
-WINDOW_PATTERN = "SSLL"
+WINDOW_PATTERN = "SSSL"
 
 # v0.1: AdamW only. Muon port is future work.
 TOTAL_BATCH_SIZE = 2**15
