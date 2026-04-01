79884bb42aedd67a1ec200dbc44a4b433822304d
Ibrahim Arshad <38925737+ibrahim1023@users.noreply.github.com>
2026-04-01 17:26:44 +0400

    chore: discard unchecked mintBatch candidate

--- contracts/gas_pack/TokenLedger.sol
+++ contracts/gas_pack/TokenLedger.sol
@@ -13,17 +13,13 @@ contract TokenLedger {
         uint256 length = accounts.length;
         if (length != amounts.length) revert LengthMismatch();
 
-        for (uint256 i = 0; i < length;) {
+        for (uint256 i = 0; i < length; ++i) {
             address account = accounts[i];
             if (account == address(0)) revert ZeroAddress();
 
             uint256 amount = amounts[i];
             balanceOf[account] += amount;
             minted += amount;
-
-            unchecked {
-                ++i;
-            }
         }
 
         totalSupply += minted;
