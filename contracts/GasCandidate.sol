bb05c2918803b4ee824cd7a0a7f649c3f4bbc7af
Ibrahim Arshad <38925737+ibrahim1023@users.noreply.github.com>
2026-04-01 16:46:48 +0400

    chore: discard unchecked retry run

--- contracts/GasCandidate.sol
+++ contracts/GasCandidate.sol
@@ -12,15 +12,12 @@ contract GasCandidate {
     function registerBatch(address[] calldata accounts) external returns (uint256 added) {
         uint256 len = accounts.length;
         address[] storage memberList = members;
-        for (uint256 i = 0; i < len;) {
+        for (uint256 i = 0; i < len; ++i) {
             address account = accounts[i];
             if (memberIndex[account] == 0) {
                 memberList.push(account);
                 memberIndex[account] = memberList.length;
                 added += 1;
-            }
-            unchecked {
-                ++i;
             }
         }
     }
@@ -35,16 +32,13 @@ contract GasCandidate {
         }
 
         uint256 runningTotal = totalScore;
-        for (uint256 i = 0; i < len;) {
+        for (uint256 i = 0; i < len; ++i) {
             address account = accounts[i];
             uint256 oldScore = scores[account];
             uint256 newScore = newScores[i];
             scores[account] = newScore;
             runningTotal = runningTotal - oldScore + newScore;
             appliedTotal += newScore;
-            unchecked {
-                ++i;
-            }
         }
         totalScore = runningTotal;
     }
@@ -53,14 +47,11 @@ contract GasCandidate {
         address[] storage memberList = members;
         uint256 len = memberList.length;
         uint256 runningTotal = totalScore;
-        for (uint256 i = 0; i < len;) {
+        for (uint256 i = 0; i < len; ++i) {
             address account = memberList[i];
             scores[account] += amount;
             runningTotal += amount;
             updated += 1;
-            unchecked {
-                ++i;
-            }
         }
         totalScore = runningTotal;
     }
