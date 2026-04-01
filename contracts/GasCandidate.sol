65dfa745a1da9c80fb6f375846c8c49ad8b0625c
Ibrahim Arshad <38925737+ibrahim1023@users.noreply.github.com>
2026-04-01 16:44:46 +0400

    chore: discard unchecked loop increment run

--- contracts/GasCandidate.sol
+++ contracts/GasCandidate.sol
@@ -11,15 +11,12 @@ contract GasCandidate {
 
     function registerBatch(address[] calldata accounts) external returns (uint256 added) {
         uint256 len = accounts.length;
-        for (uint256 i = 0; i < len;) {
+        for (uint256 i = 0; i < len; ++i) {
             address account = accounts[i];
             if (!registered[account]) {
                 registered[account] = true;
                 members.push(account);
                 added += 1;
-            }
-            unchecked {
-                ++i;
             }
         }
     }
@@ -34,16 +31,13 @@ contract GasCandidate {
         }
 
         uint256 runningTotal = totalScore;
-        for (uint256 i = 0; i < len;) {
+        for (uint256 i = 0; i < len; ++i) {
             address account = accounts[i];
             uint256 oldScore = scores[account];
             uint256 newScore = newScores[i];
             scores[account] = newScore;
             runningTotal = runningTotal - oldScore + newScore;
             appliedTotal += newScore;
-            unchecked {
-                ++i;
-            }
         }
         totalScore = runningTotal;
     }
@@ -51,14 +45,11 @@ contract GasCandidate {
     function bumpAll(uint256 amount) external returns (uint256 updated) {
         uint256 len = members.length;
         uint256 runningTotal = totalScore;
-        for (uint256 i = 0; i < len;) {
+        for (uint256 i = 0; i < len; ++i) {
             address account = members[i];
             scores[account] += amount;
             runningTotal += amount;
             updated += 1;
-            unchecked {
-                ++i;
-            }
         }
         totalScore = runningTotal;
     }
