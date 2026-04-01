085c6e6bc84165ba64c82d49ebd15a3c2ade3f79
Ibrahim Arshad <38925737+ibrahim1023@users.noreply.github.com>
2026-04-01 16:46:17 +0400

    experiment: retry unchecked loop increments on index mapping

--- contracts/GasCandidate.sol
+++ contracts/GasCandidate.sol
@@ -12,12 +12,15 @@ contract GasCandidate {
     function registerBatch(address[] calldata accounts) external returns (uint256 added) {
         uint256 len = accounts.length;
         address[] storage memberList = members;
-        for (uint256 i = 0; i < len; ++i) {
+        for (uint256 i = 0; i < len;) {
             address account = accounts[i];
             if (memberIndex[account] == 0) {
                 memberList.push(account);
                 memberIndex[account] = memberList.length;
                 added += 1;
+            }
+            unchecked {
+                ++i;
             }
         }
     }
@@ -32,13 +35,16 @@ contract GasCandidate {
         }
 
         uint256 runningTotal = totalScore;
-        for (uint256 i = 0; i < len; ++i) {
+        for (uint256 i = 0; i < len;) {
             address account = accounts[i];
             uint256 oldScore = scores[account];
             uint256 newScore = newScores[i];
             scores[account] = newScore;
             runningTotal = runningTotal - oldScore + newScore;
             appliedTotal += newScore;
+            unchecked {
+                ++i;
+            }
         }
         totalScore = runningTotal;
     }
@@ -47,11 +53,14 @@ contract GasCandidate {
         address[] storage memberList = members;
         uint256 len = memberList.length;
         uint256 runningTotal = totalScore;
-        for (uint256 i = 0; i < len; ++i) {
+        for (uint256 i = 0; i < len;) {
             address account = memberList[i];
             scores[account] += amount;
             runningTotal += amount;
             updated += 1;
+            unchecked {
+                ++i;
+            }
         }
         totalScore = runningTotal;
     }
