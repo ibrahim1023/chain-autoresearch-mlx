ee43f7a0b4e2426e77840b611a22b9a08694db42
Ibrahim Arshad <38925737+ibrahim1023@users.noreply.github.com>
2026-04-01 17:27:30 +0400

    experiment: load mintBatch inputs from calldata

--- contracts/gas_pack/TokenLedger.sol
+++ contracts/gas_pack/TokenLedger.sol
@@ -13,13 +13,22 @@ contract TokenLedger {
         uint256 length = accounts.length;
         if (length != amounts.length) revert LengthMismatch();
 
-        for (uint256 i = 0; i < length; ++i) {
-            address account = accounts[i];
+        for (uint256 i = 0; i < length;) {
+            address account;
+            uint256 amount;
+            assembly {
+                account := calldataload(add(accounts.offset, shl(5, i)))
+                amount := calldataload(add(amounts.offset, shl(5, i)))
+            }
+
             if (account == address(0)) revert ZeroAddress();
 
-            uint256 amount = amounts[i];
             balanceOf[account] += amount;
             minted += amount;
+
+            unchecked {
+                ++i;
+            }
         }
 
         totalSupply += minted;
